de37412688e4b688cfec0e4f6f06974321672ab9
敛馀辉 <agzhh@qq.com>
2023-08-12 11:02:08 +0800

    fix: 🧩 修复路由缓存嵌套不缓存问题

--- src/layout/page-view.vue
+++ src/layout/page-view.vue
@@ -1,8 +1,8 @@
 <template>
 	<router-view v-slot="{ Component, route }">
 		<transition name="fade" mode="out-in" appear>
-			<component :is="Component" v-if="route.meta.ignoreCache" :key="route.fullPath" />
-			<keep-alive v-else :include="cacheList">
+			<!--			<component :is="Component" v-if="route.meta.ignoreCache" :key="route.fullPath" />-->
+			<keep-alive :include="cacheList">
 				<component :is="Component" :key="route.fullPath" />
 			</keep-alive>
 		</transition>
@@ -18,4 +18,60 @@ const tabBarStore = useTabBarStore();
 const cacheList = computed(() => tabBarStore.getCacheList);
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.3s;
+}
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+	transition: all 0.3s ease-out;
+}
+
+.slide-enter-to {
+	position: absolute;
+	right: 0;
+}
+
+.slide-enter-from {
+	position: absolute;
+	right: -100%;
+}
+
+.slide-leave-to {
+	position: absolute;
+	left: -100%;
+}
+
+.slide-leave-from {
+	position: absolute;
+	left: 0;
+}
+
+.scale-slide-enter-active,
+.scale-slide-leave-active {
+	position: absolute;
+	transition: all 0.5s ease;
+}
+
+.scale-slide-enter-from {
+	left: -100%;
+}
+
+.scale-slide-enter-to {
+	left: 0%;
+}
+
+.scale-slide-leave-from {
+	transform: scale(1);
+}
+
+.scale-slide-leave-to {
+	transform: scale(0.8);
+}
+</style>
