95687640ef42ed2e81f18e63a91f7ab33b4d045d
敛馀辉 <agzhh@qq.com>
2024-01-12 09:19:57 +0800

    fix: 🧩 修复路由问题

--- src/router/routes/custom.js
+++ src/router/routes/custom.js
@@ -1,9 +1,5 @@
 // 当前文档是用来配置一下自定义的路由
 
-export const ROUTE_WHITELIST = ["openInventory"];
+export const ROUTE_WHITELIST = [];
 
-export const CUSTOM_ROUTES = [
-	{
-		name: "openInventory"
-	}
-];
+export const CUSTOM_ROUTES = [];
